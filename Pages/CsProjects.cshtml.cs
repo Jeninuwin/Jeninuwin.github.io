ee0a8c0e0489c095fd725b0abbe380a69390ab48
Jenny Nguyen <44487057+Jeninuwin@users.noreply.github.com>
2021-10-19 20:38:25 -0500

    Add files via upload

---
+++ Pages/CsProjects.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Pages
+{
+    public class ProjectModel : PageModel
+    {
+        private readonly ILogger<ProjectModel> _logger;
+
+        public ProjectModel(ILogger<ProjectModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Graphics.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.Pages
+{
+    public class GraphicModel : PageModel
+    {
+        private readonly ILogger<GraphicModel> _logger;
+
+        public GraphicModel(ILogger<GraphicModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
